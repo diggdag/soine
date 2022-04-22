a9da5fd02baa4b44669454c8376b9a1718ab387f
RyoKurachi <ryokurachiryo@gmail.com>
2022-04-22 22:04:27 +0900

    Initial Commit

---
+++ soine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  soine
+//
+//  Created by 倉知諒 on 2022/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
